c396d1797c2d2a1578bc9503b9252d5ed5b3b617
luis lagos <luislagos62@gmail.com>
2024-03-07 21:44:28 -0300

    ejercicios de python y notas

--- dia 1/ejercicio1.py
+++ dia 1/ejercicio1.py
@@ -1,6 +1,8 @@
+# hoja para hacer anotaciones
+
 # gato para comentario de una linea 
+#python no permite sumar letras y numeros
+#suma de string es concatenar 
 """
     doble comillas para comentarios en bloques 
 """
-
-print("holas tortolas xdddd ")

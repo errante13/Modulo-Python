c4e25fda53def16695d47e9d38405e75d0b24d08
luis lagos <luislagos62@gmail.com>
2024-03-08 22:04:45 -0300

    mas notas y ejercicios

---
+++ dia 2/librerias.py
@@ -0,0 +1,16 @@
+"""
+LAS IMPORTACIONES VAN EN LAS PRIMERAS LINEAS DEL CODIGO
+"""
+import os
+#import math
+from math import ceil,sqrt
+print (2**4) # ** elevado 2**4 2 elevado a 4 
+print (5//2) # // division de enteros 5//2 es 2 
+print(5%2)   # %  modulo para saber el resto 5 % 2 es 1  
+
+print (os.getcwd()) #muestra la ruta donde estoy parado en la computadora
+print (sqrt(2))# ejemplo de usar librerias, en este caso uso de función raiz cuadrada de math 
+print (ceil(3.1416))# se usa de esta forma para importar de especifica
+# print (math.sqrt(2)) #se usa de esta forma cuando se importa la librería completa
+
+# pip -v para saber la versión 

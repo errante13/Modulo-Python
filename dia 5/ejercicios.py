c20ab76f1bed392d7998c2484a7f808e09eae480
luis lagos <luislagos62@gmail.com>
2024-03-13 21:54:00 -0300

    clase dia 5

---
+++ dia 5/ejercicios.py
@@ -0,0 +1,16 @@
+#variables 
+edad =27
+#¿ Juan es mayor de edad ?
+#print 
+print(edad>=18)
+
+
+#se agrega edad para una mejor compresión
+edad_graduacion_colegio=17
+#Juan se graduó antes de los 18?
+print(edad_graduacion_colegio<18)#true
+print(edad_graduacion_colegio>=18)#false
+
+nombre="luis"
+me_llamo_luis= nombre =="luis"
+print(me_llamo_luis)

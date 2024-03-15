e2ede86cf8359b088d50195744499a061242ae6e
luis lagos <luislagos62@gmail.com>
2024-03-15 15:26:31 -0300

    desafio evaluado 2

---
+++ desafio 2/cachipun.py
@@ -0,0 +1,73 @@
+"""
+Para poner en práctica lo que hemos aprendido a lo largo de la unidad, se implementará un
+programa en Python que permite jugar al cachipún en contra del computador.
+
+"""
+#import libreria random
+import random
+
+seleccion_usuario=input("ingrese piedra, papel o tijeras para comenzar a juegar \n").lower()
+
+#validación de datos del usuario
+if seleccion_usuario =="piedra":
+    print(f"tu jugaste {seleccion_usuario}")
+elif seleccion_usuario =="tijera":
+    print(f"tu jugaste {seleccion_usuario}")
+elif seleccion_usuario =="papel":
+    print(f"tu jugaste {seleccion_usuario}")
+else:
+    print("la opción escogida es INVALIDAD, favor es cojer entre piedra, papel o tijera\n")    
+"""
+POSIBLES CONVINACIONES 
+ PC    USER 
+ "PIEDRA PIEDRA "
+ PIEDRA TIJERA--
+ PIEDRA PAPEL--
+ "TIJERA TIJERA" 
+ TIJERA PAPEL --
+ TIJERA PIEDRA--
+ "PEPEL PAPEL"
+ PAPEL PIEDRA--
+ PAPEL TIJERAS-- 
+"""
+
+#creación de una opción aleatoria para el pc
+computador=random.choice(["piedra","papel","tijera"])
+
+#caso de empate
+if computador==seleccion_usuario:
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n ")
+    print("es un EMPATE \n")
+# piedra tijeras    
+elif computador =="piedra"and seleccion_usuario=="tijera":
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n ")
+    print("PERDISTE piedra rompe tijeras\n")
+ #papel tijeras   
+elif computador =="papel"and seleccion_usuario=="tijera":
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n ")
+    print("GANASTE tijeras cortan papel\n")
+ #tijeras papel   
+elif computador =="tijeras" and seleccion_usuario=="papel":
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n ")
+    print("PERDISTE tijeras cortan papel\n")
+#piedra papel
+elif computador =="piedra"and seleccion_usuario=="papel":
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n")
+    print("GANASTE papel envuelve a piedra\n")
+#tijera piedra
+elif computador =="tijera"and seleccion_usuario=="piedra":
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n ")
+    print("GANASTE piedra rompe tijera\n")
+#papel piedra
+elif computador =="papel"and seleccion_usuario=="piedra":
+    print(f"El computador eligió: {computador}\n")
+    #print(f"Tu jusgaste {seleccion_usuario}\n ")
+    print("PERDISTE papel envuelve a piedra\n")
+
+print("fin del juego")    
---
+++ desafio 2/imc.py
@@ -0,0 +1,40 @@
+"""
+EJERCICIO 1
+
+se le solicita a usted como programador el hecho de diseñar una
+herramienta que permita determinar el estado nutricional de una persona.
+
+"""
+#VALORES ENTREGADOS POR EL USUARIO
+peso_persona=float(input("ingrese el peso de la persona en KG\n"))
+altura_persona=float(input("ingrese la altura de la persona en centimetros y distinto de cero (0) \n"))/100
+
+#CALCULO IMC
+imc=round(peso_persona/pow(altura_persona,2),2)
+
+#CLASIFICACIÓN SEGUN TABLA OMS
+
+"""
+< 18.5        Bajo Peso
+[ 18.5,25 [   Adecuado
+[ 25,  30 [   Sobrepeso
+[ 30,  35 [   Obesidad Grado I
+[ 35,  40 [   Obesidad Grado II
+> 40          Obesidad Grado III
+"""
+
+if imc<18.5:
+    print(f"su IMC es de {imc} y su clasificación según la OMS es de BAJO PESO\n")
+elif imc >=18.5 and imc<25:
+    print(f"su IMC es de {imc} y su clasificación según la OMS es de ADECUADO\n")
+elif imc>=25 and imc <30:
+    print(f"su IMC es de {imc} y su clasificación según la OMS es de SOBREPESO\n")
+elif imc>=30 and imc <35:
+    print(f"su IMC es de {imc} y su clasificación según la OMS es de OBESIDAD GRADO 1\n") 
+elif imc>=35 and imc <40:
+    print(f"su IMC es de {imc} y su clasificación según la OMS es de OBESIDAD GRADO 2\n") 
+elif imc>=40:
+    print(f"su IMC es de {imc} y su clasificación según la OMS es de OBESIDAD GRADO 3\n") 
+
+
+print("TERMINO DEL PROGRAMA")

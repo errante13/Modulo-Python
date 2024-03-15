37ed5c44f69722a7e96b0d35f08c504f5cdcc628
luis lagos <luislagos62@gmail.com>
2024-03-15 12:44:02 -0300

    clase dia 6

---
+++ dia 6/ejercicios6.py
@@ -0,0 +1,9 @@
+
+valor = int(input("ingrese un numero entero a probar\n"))
+
+if valor % 2 == 0:
+    print ("el número ingresado es par")
+else:
+    print ("el número ingresado es impar")
+
+print("fin de la ejecución")
---
+++ dia 6/sentencias.py
@@ -0,0 +1,9 @@
+edad = input("¿Que edad tienes?\n")#17 -> se almacena "17"
+edad = int(edad)# edad = int("17")-> edad = 17
+
+#Si el usuario es mayor de edad o menor de edad
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+print("Programa terminado") 

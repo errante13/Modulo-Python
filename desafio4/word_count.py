ec8074724959937fa4dc6c77f77a34ed50a5439e
luis lagos <luislagos62@gmail.com>
2024-03-22 19:41:08 -0300

    corrección nombre carpetas

--- desafio4/word_count.py
+++ desafio4/word_count.py
@@ -1,7 +1,7 @@
-# from sys import argv
-# argumento = argv[1]
+from sys import argv
+argumento = argv[1]
 
-with open("Modulo-Python/desafio 4/lorem_ipsum.txt", "r") as file:
+with open(argumento, "r") as file:
     texto=file.read()
     
 print(" -------- INICIO PROGRAMA ------- \n")

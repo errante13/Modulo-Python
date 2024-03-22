d51dcf5c7eb3a71cdd049626b91f4b871e168bc3
luis lagos <luislagos62@gmail.com>
2024-03-22 16:58:14 -0300

    desafio 4

---
+++ desafio 4/conversiones.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+print(" ------ INICIO ------ ")
+#monedas
+
+sol_peruano = float(argv[1])
+peso_argentino =float(argv[2])
+dolar_americano =float(argv[3])
+peso_chileno = float(argv[4])
+
+#conversiones 
+
+print("")
+
+print(f"Los {round(peso_chileno)} pesos equivalen a: ")
+print(f"{sol_peruano*peso_chileno} Soles ")
+print(f"{peso_argentino*peso_chileno} Pesos Argentinos ")
+print(f"{dolar_americano*peso_chileno} Dólares ")
+
+print("")
+print(" ------ FIN ------ ")
---
+++ desafio 4/word_count.py
@@ -0,0 +1,28 @@
+
+with open("Modulo-Python/desafio 4/lorem_ipsum.txt", "r") as file:
+    texto=file.read()
+print(" -------- INICIO PROGRAMA ------- \n")
+
+#contador de letras 
+""" 
+    
+letras =[]
+for i in texto:
+    letras.append(i)
+    
+"""
+# ----- OPTIMIZACIÓN DEL CICLO Y LISTA ------
+letras =[i for i in texto]
+letras_convertidas =set(letras)
+# print (type(letras_convertidas))
+print (f"El número de caracteres distintos es: {len(letras_convertidas)}")    
+
+#contador de palabras
+
+palabras = texto.split(" ")
+# print(palabras)
+palabras_convertidas =set(palabras)
+# print (type(palabras_convertidas))
+print (f"El número de palabras distintas es: {len(palabras_convertidas)}\n")    
+
+print(" ------- FIN PROGRAMA ------- ")
